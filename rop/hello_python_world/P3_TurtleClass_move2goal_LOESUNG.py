ab9f0800454763130afe973359c6e9e19ccdf70c
Prof. Just <olaf.just@w-hs.de>
2019-10-21 17:47:57 +0200

    Add files via upload

--- rop/hello_python_world/P3_TurtleClass_move2goal_LOESUNG.py
+++ rop/hello_python_world/P3_TurtleClass_move2goal_LOESUNG.py
@@ -18,14 +18,13 @@ class TurtleBotClass:
 		rospy.init_node('turtlebot_controller', anonymous=True)
 
 		# Publisher which will publish to the topic '/turtle1/cmd_vel'.
-		self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel',
-												  Twist, queue_size=10)
+		self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 
 		# A subscriber to the topic '/turtle1/pose'. self.update_pose is called
 		# when a message of type Pose is received.
-		self.pose_subscriber = rospy.Subscriber('/turtle1/pose',
-												Pose, self.update_pose)
-
+		self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.update_pose)
+		
+		# globale Variablen instanzieren
 		self.pose = Pose()
 		self.rate = rospy.Rate(10)
 
@@ -40,17 +39,14 @@ class TurtleBotClass:
 		"""Euclidean distance between current pose and the goal."""
 		return sqrt(pow((goal_pose.x - self.pose.x), 2) +
 					pow((goal_pose.y - self.pose.y), 2))
-
+					
 	def linear_vel(self, goal_pose, constant=1.5):
-		"""See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
 		return constant * self.euclidean_distance(goal_pose)
 
 	def steering_angle(self, goal_pose):
-		"""See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
 		return atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x)
 
 	def angular_vel(self, goal_pose, constant=6):
-		"""See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
 		return constant * (self.steering_angle(goal_pose) - self.pose.theta)
 
 	def move2goal(self):
