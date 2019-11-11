edea0ad0406390a897007ffc90748159fb49c612
Prof. Just <olaf.just@w-hs.de>
2019-11-11 14:02:44 +0100

    Add files via upload

--- rop_sw_Kap10_QtDesigner_www/sw03_Text_Server_mit _Flask.py
+++ rop_sw_Kap10_QtDesigner_www/sw03_Text_Server_mit _Flask.py
@@ -1,10 +1,18 @@
 ### Simpler Flask Webserver
 ### Bsp aus https://www.youtube.com/watch?v=vyCboBjK4us 2:48
 
-### ----------------------------------
-### $sudo pip install flask
-### ----------------------------------
-### starten als $sudo sw03_Text_Server_mit _Flask.py
+""" ----------------------------------
+# ggf. PIP installieren
+# $ sudo apt install python-pip
+#----------------------------------
+# ggf.  Apache Server stoppen
+# $systemctl stop apache2.service
+# ----------------------------------
+# $sudo pip install flask
+# ----------------------------------
+#starten als 
+# $sudo sw03_Text_Server_mit _Flask.py
+"""
 
 from flask import Flask
 
@@ -17,3 +25,4 @@ def main():
 
 if __name__=="__main__":
 	app.run(debug=True, host="127.0.0.1", port=80)
+
